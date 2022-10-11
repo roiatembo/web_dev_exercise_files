bb91b27fc4e4902c57aaecbd4510c3dd7b19c3a9
Roia Langa Tembo <roia@kali.theitmogul.co.za>
2022-10-11 12:00:15 +0200

    node js using npm packages

---
+++ intro_to_node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes-name");
+var mySupe = superheroes.random();
+console.log(mySupe);
+
+var supervillains = require("supervillains");
+var myVillain = supervillains.random();
+console.log(myVillain);
