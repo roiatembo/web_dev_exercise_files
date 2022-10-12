4690b94a631b1744a87eaf6e12ef293d8d124d80
Roia Langa Tembo <roia@kali.theitmogul.co.za>
2022-10-12 10:51:52 +0200

    bmi calculator challenge

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -8,12 +8,25 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html")
 });
 
+app.get("/bmiCalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function(req, res){
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
 
     var result = num1 + num2;
     res.send("the result is " + result);
+})
+
+app.post("/bmiCalculator", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var bmiResult = weight / (height * height);
+    res.send("Your BMI is " + bmiResult);
+
 })
 
 app.listen(3000, function() {
