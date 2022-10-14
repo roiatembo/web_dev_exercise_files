66df31b69a97fb3d4123340f39d219a8fe2be6c7
Roia Langa Tembo <roia@kali.theitmogul.co.za>
2022-10-14 12:00:17 +0200

    newsletter project created

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -4,20 +4,25 @@ const https = require("https")
 const app = express();
 
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=-23.848281&lon=29.414443&appid=773f78fa0d3317bd4f712fa38c44bc50"
-    https.get(url, function(r) {
-        console.log(r.statusCode)
 
-        r.on("data", function(data) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&lat=-23.848281&lon=29.414443&appid=773f78fa0d3317bd4f712fa38c44bc50"
+    https.get(url, function(rs) {
+        console.log(rs.statusCode)
+
+        rs.on("data", function(data) {
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const weatherDescription = weatherData.weather[0].description
-            console.log(weatherDescription)
+            const city = weatherData.name
+            const icon = weatherData.weather[0].icon
+            const imageURL = "https://openweathermap.org/img/wn/"+ icon + "@2x.png"
+            res.write("<h1>The temperature in " + city + " is " + temp + " degrees Celcius</h1><br>")
+            res.write("<p>The weather is currently " + weatherDescription + "</p>")
+            res.write("<img src=" + imageURL + ">")
+            res.send()
         })
     })
 
-
-    res.send("Server is up and running")
 })
 
 
